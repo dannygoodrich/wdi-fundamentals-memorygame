6e55e1bc36ebe8bd5052879001e08042c7963e5b
Daniel Goodrich <danielgoodrich@Daniels-MacBook-Air.local>
2020-01-09 19:40:57 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,15 +3,17 @@ console.log("Up and running!");
 
 let cards = ["queen", "queen", "king", "king"];
 let cardsInPlay = [];
-let cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-let cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
+function checkForMatch() {
+	if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
 		} else {
-			alert("Sorry, try again.");
+			console.log("Sorry, try again.");
 		}
-	
+}
+function flipCard (cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+}
+flipCard(0);
+flipCard(2);
