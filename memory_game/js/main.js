2479f0e778630eaa5edbddb4c324863881a589bf
Daniel Goodrich <danielgoodrich@Daniels-MacBook-Air.local>
2020-01-12 02:45:30 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -24,19 +24,53 @@ let cards = [
 }
 ];
 let cardsInPlay = [];
+
 function checkForMatch() {
-	if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+	
+	if (cardsInPlay[0] === cardsInPlay[1]) {
 		console.log("You found a match!");
+		alert("You found a match!");
 		} else {
 			console.log("Sorry, try again.");
+			alert("Sorry, try again.");
 		}
-}
-function flipCard (cardId) {
+	}
+
+function flipCard () {
+	var cardId = this.getAttribute('data-id');
+	this.setAttribute('src', cards[cardId].cardImage);
 	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
-	checkForMatch();
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
+	
 }
-flipCard(0);
-flipCard(2);
+
+
+function createBoard() {
+	for (let i = 0; i < cards.length; i++) {
+		let cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+	}
+
+
+
+createBoard();
+
+
+
+
+
+
+
+
+
+
+
